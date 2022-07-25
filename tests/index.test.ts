42bc10f8339208e0cefc1dd6b595c8b6032363a0
Subhajit Sahu <wolfram77@gmail.com>
2022-07-25 19:33:35 +0530

    :mango: w/ more tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,4 +1,7 @@
-import * as path from "../src";
+import * as path     from "../src";
+import {filename}    from "../src";
+import {symbolname}  from "../src";
+import {keywordname} from "../src";
 
 
 
@@ -16,4 +19,36 @@ test("example1", () => {
   var a = path.keywordname("/home/user/file+name.txt");
   expect(a).toBe("file-name");
   // → file-name
+});
+
+
+
+
+test("filename", () => {
+  var a = filename("/home/user/open+source.txt");
+  expect(a).toBe("open+source");
+  var a = filename("/home/user/closedSource.key");
+  expect(a).toBe("closedSource");
+  var a = filename("/home/user/copyleft_licenses.tar.gz");
+  expect(a).toBe("copyleft_licenses.tar");
+});
+
+
+test("symbolname", () => {
+  var a = symbolname("/home/user/open+source.txt");
+  expect(a).toBe("open_source");
+  var a = symbolname("/home/user/closedSource.key");
+  expect(a).toBe("closedSource");
+  var a = symbolname("/home/user/copyleft_licenses.tar.gz");
+  expect(a).toBe("copyleft_licenses_tar");
+});
+
+
+test("keywordname", () => {
+  var a = keywordname("/home/user/open+source.txt");
+  expect(a).toBe("open-source");
+  var a = keywordname("/home/user/closedSource.key");
+  expect(a).toBe("closed-source");
+  var a = keywordname("/home/user/copyleft_licenses.tar.gz");
+  expect(a).toBe("copyleft-licenses-tar");
 });
